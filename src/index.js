aad216c047bb6f7b92a03e014f053c7fb00e8195
CharlieLau <27865263@qq.com>
2020-08-07 00:31:37 +0800

    fix: jsx event-name adjust

--- src/index.js
+++ src/index.js
@@ -34,6 +34,7 @@ const components = [
   ButtonGroup,
   Popover,
   Tooltip,
+  Tag,
   Alert,
   Icon,
   Row,
@@ -48,15 +49,15 @@ const components = [
   Link,
   Backtop,
   CollapseTransition
-]
+];
 
-const install = function(app, opts = {}) {
+const install = function (app, opts = {}) {
   // locale.use(opts.locale);
   // locale.i18n(opts.i18n);
 
   components.forEach(component => {
-    app.component(component.name, component)
-  })
+    app.component(component.name, component);
+  });
 
   // app.use(InfiniteScroll);
   // app.use(Loading.directive);
@@ -64,7 +65,7 @@ const install = function(app, opts = {}) {
   app.config.globalProperties.$ELEMENT = {
     size: opts.size || '',
     zIndex: opts.zIndex || 2000
-  }
+  };
 
   // app.config.globalProperties.$loading = Loading.service;
   // app.config.globalProperties.$msgbox = MessageBox;
@@ -73,11 +74,11 @@ const install = function(app, opts = {}) {
   // app.config.globalProperties.$prompt = MessageBox.prompt;
   // app.config.globalProperties.$notify = Notification;
   // app.config.globalProperties.$message = Message;
-}
+};
 
 /* istanbul ignore if */
 if (typeof window !== 'undefined' && window.Vue) {
-  install(window.Vue)
+  install(window.Vue);
 }
 
 export default {
@@ -96,6 +97,7 @@ export default {
   ButtonGroup,
   Popover,
   Tooltip,
+  Tag,
   Alert,
   Icon,
   Row,
@@ -109,4 +111,4 @@ export default {
   Footer,
   Link,
   Backtop
-}
+};
