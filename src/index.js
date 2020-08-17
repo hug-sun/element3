6b7e99245a56f0fa901ebf9c8ba5da69019bddb9
wxy <wxy306533309@163.com>
2020-08-17 16:47:46 +0800

    refactor: progress component

--- src/index.js
+++ src/index.js
@@ -16,6 +16,7 @@ import Alert from '../packages/alert/index.js';
 import Icon from '../packages/icon/index.js';
 import Row from '../packages/row/index.js';
 import Col from '../packages/col/index.js';
+import Progress from '../packages/progress/index.js';
 import Badge from '../packages/badge/index.js';
 import Carousel from '../packages/carousel/index.js';
 import Scrollbar from '../packages/scrollbar/index.js';
@@ -48,6 +49,7 @@ const components = [
   Icon,
   Row,
   Col,
+  Progress,
   Badge,
   Carousel,
   Scrollbar,
@@ -116,6 +118,7 @@ export default {
   Icon,
   Row,
   Col,
+  Progress,
   Badge,
   Carousel,
   Scrollbar,
