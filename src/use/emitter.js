702bc74dea68f95a2564fd7c7d95457e298529b1
yeyuqiudeng <yeyuqiudeng@gmail.com>
2020-08-20 21:56:22 +0800

    fix: should check all childComponent

--- src/use/emitter.js
+++ src/use/emitter.js
@@ -102,9 +102,8 @@ function broadcast() {
       if (children) {
         children.forEach((vnode) => {
           const childComponent = vnode.component
-          if (!childComponent) return
 
-          if (childComponent.type.name === componentName) {
+          if (childComponent && childComponent.type && childComponent.type.name === componentName) {
             childComponent.emit(eventName, ...params)
           } else {
             _broadcast(childComponent)
