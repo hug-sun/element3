4a52616e745b63766cb64d12d99e3748738ef464
任文慧 <renwenhui@renwenhuideMacBook-Pro.local>
2020-08-31 07:25:44 +0800

    git checkout  -b  ‘InfiniteScroll.vue’

--- packages/infinite-scroll_old/index.js
+++ packages/infinite-scroll_old/index.js
@@ -1,7 +1,8 @@
-import InfiniteScroll from './InfiniteScroll'
+import InfiniteScroll from './src/main.js'
 
 /* istanbul ignore next */
 InfiniteScroll.install = function (app) {
   app.directive(InfiniteScroll.name, InfiniteScroll)
 }
+
 export default InfiniteScroll
