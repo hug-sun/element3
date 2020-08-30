17571d282f6bfb75dcb5c059667ebe65b2ad520b
MengJia <jiameng15@126.com>
2020-08-30 10:42:08 +0800

    test: backtop

--- packages/backtop/index.js
+++ packages/backtop/index.js
@@ -1,8 +1,9 @@
-import ElBacktop from './Backtop'
+import Backtop from './src/main'
+// import Backtop from './Backtop'
 
 /* istanbul ignore next */
-ElBacktop.install = function (app) {
-  app.component(ElBacktop.name, ElBacktop)
+Backtop.install = function (app) {
+  app.component(Backtop.name, Backtop)
 }
 
-export default ElBacktop
+export default Backtop
