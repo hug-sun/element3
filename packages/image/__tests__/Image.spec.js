d4f22e5a6f5c4bb954d893c146c20f0b625ec226
Qiyuan <mqycn@126.com>
2020-09-01 01:05:31 +0800

    test: Add Image

--- packages/image/__tests__/Image.spec.js
+++ packages/image/__tests__/Image.spec.js
@@ -1,19 +1,93 @@
 import Image from '../Image.vue'
-import { mount } from '@vue/test-utils'
+import { mount as _mount } from '@vue/test-utils'
+
+const mount = async (Component, options) => {
+  const wrapper = _mount(Component, options)
+  wrapper.componentVM.$isServer = false
+  wrapper.componentVM.loading = false
+  return new Promise((resolve) => {
+    setTimeout(() => {
+      resolve(wrapper)
+    }, 0)
+  })
+}
+
+Image.$isServer = false
 
 describe('Image.vue', () => {
-  describe('src', () => {
-    const wrapper = mount(Image, {
-      props: {
-        src: 'test.jpg'
-      }
-    })
-    wrapper.componentVM.$isServer = false
-    wrapper.componentVM.loading = false
-    it('load', () => {
+  describe('Attributes', () => {
+    it('src', async () => {
+      const wrapper = await mount(Image, {
+        props: {
+          src: 'test.jpg'
+        }
+      })
       expect(wrapper.find('.el-image__inner').attributes('src')).toBe(
         'test.jpg'
       )
+    })
+
+    it('fit', async () => {
+      const wrapper = await mount(Image, {
+        props: {
+          fit: 'fill'
+        }
+      })
+      expect(wrapper.find('.el-image__inner').attributes('style')).toContain(
+        'object-fit: fill;'
+      )
+    })
+
+    it('alt', async () => {
+      const wrapper = await mount(Image, {
+        props: {
+          alt: 'hello world!'
+        }
+      })
+      expect(wrapper.find('.el-image__inner').attributes('alt')).toBe(
+        'hello world!'
+      )
+    })
+
+    it('referrer-policy', async () => {
+      // TODO
+    })
+
+    it('lazy', async () => {
+      // TODO
+    })
+
+    it('scroll-container', async () => {
+      // TODO
+    })
+
+    it('preview-src-list', async () => {
+      // TODO
+    })
+
+    it('z-index', async () => {
+      // TODO
+    })
+  })
+  describe('Events', () => {
+    it('load', async () => {
+      // TODO
+    })
+
+    it('error', async () => {
+      // TODO
+    })
+  })
+
+  describe('Slots', () => {
+    // Slots
+
+    it('placeholder', async () => {
+      // TODO
+    })
+
+    it('error', async () => {
+      // TODO
     })
   })
 })
