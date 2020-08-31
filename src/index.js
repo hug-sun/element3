4576d4420bf62046ec32334d52ea165bbb5af9ad
严畅 <yanchang@segimail.com>
2020-08-31 10:49:54 +0800

    refactor: refactor steps step

--- src/index.js
+++ src/index.js
@@ -25,6 +25,8 @@ import Card from '../packages/card/index.js';
 import Rate from '../packages/rate/index.js';
 import Carousel from '../packages/carousel/index.js';
 import Scrollbar from '../packages/scrollbar/index.js';
+import Steps from '../packages/steps/index.js';
+import Step from '../packages/step/index.js';
 import Container from '../packages/container/index.js';
 import Header from '../packages/header/index.js';
 import Aside from '../packages/aside/index.js';
@@ -73,7 +75,9 @@ const components = [
   Divider,
   Backtop,
   Avatar,
-   CollapseTransition
+  CollapseTransition,
+  Steps,
+  Step
 ];
 
 const install = function(app, opts = {}) {
@@ -147,5 +151,7 @@ export default {
   Link,
   Divider,
   Backtop,
-  Avatar
+  Avatar,
+  Steps,
+  Step
 };
