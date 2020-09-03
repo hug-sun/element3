36ae4a829cef3549546b3fb8c512f71c78751148
scarsu <scarsu001@gmail.com>
2020-09-03 12:26:02 +0800

    refactor: refactor Popconfirm and update test

--- build/bin/build-entry.js
+++ build/bin/build-entry.js
@@ -115,7 +115,8 @@ ComponentNames.forEach((name) => {
       'page-header',
       'message',
       'step',
-      'steps'
+      'steps',
+      'popconfirm'
     ].indexOf(name) > -1
   ) {
     // 白名单 挨个替换
