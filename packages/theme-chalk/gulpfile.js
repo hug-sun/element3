bb731c16f7c86dd6a49ba52acf01ccc605dfaf1b
erye <erye@tiiit.cn>
2020-09-06 23:49:47 +0800

    feat: replace node-sass with sass

--- packages/theme-chalk/gulpfile.js
+++ packages/theme-chalk/gulpfile.js
@@ -1,16 +1,31 @@
 'use strict'
 
 const { series, src, dest } = require('gulp')
-const sass = require('gulp-sass')
+const sass = require('sass')
 const autoprefixer = require('gulp-autoprefixer')
 const cssmin = require('gulp-cssmin')
+const through2 = require('through2')
+const Fiber = require('fibers')
+const replaceExt = require('replace-ext')
 
 function compile() {
   return src('./src/*.scss')
-    .pipe(sass.sync())
+    .pipe(
+      through2.obj(function (file, enc, cb) {
+        const result = sass.renderSync({ file: file.path, fiber: Fiber })
+        file.contents = result.css
+        file.path = replaceExt(file.path, '.css')
+
+        if (file.stat) {
+          file.stat.atime = file.stat.mtime = file.stat.ctime = new Date()
+        }
+
+        cb(null, file)
+      })
+    )
     .pipe(
       autoprefixer({
-        browsers: ['ie > 9', 'last 2 versions'],
+        overrideBrowserslist: ['ie > 9', 'last 2 versions'],
         cascade: false
       })
     )
