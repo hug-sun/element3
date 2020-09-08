e3f2665dcd7935e11a6632e6336b213b72c9f005
cuixiaorui <cui_xiaorui@126.com>
2020-09-08 19:31:46 +0800

    chore: export breadcrumb component

--- src/entry.js
+++ src/entry.js
@@ -4,9 +4,17 @@ import ElSwitch from '../packages/switch'
 import ElProgress from '../packages/progress'
 import ElLink from '../packages/link'
 import ElIcon from '../packages/icon'
+import ElBreadcrumb from '../packages/breadcrumb'
 import { version } from '../package.json'
 
-const components = [ElButton, ElSwitch, ElProgress, ElLink, ElIcon]
+const components = [
+  ElButton,
+  ElSwitch,
+  ElProgress,
+  ElLink,
+  ElIcon,
+  ElBreadcrumb
+]
 
 const install = (app) => {
   components.forEach((component) => {
@@ -19,6 +27,6 @@ const elementUI = {
   install
 }
 
-export { ElButton, ElSwitch, ElProgress, ElLink, ElIcon }
+export { ElButton, ElSwitch, ElProgress, ElLink, ElIcon, ElBreadcrumb }
 
 export default elementUI
