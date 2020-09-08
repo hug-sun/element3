1444ea52eab4e3d9825416a7ec670a73d6acbfa8
yt0379 <yt0379@qq.com>
2020-09-08 14:00:31 +0800

    fix: clickoutside指令失效

--- packages/select/Select.vue
+++ packages/select/Select.vue
@@ -172,7 +172,7 @@ import ElOption from 'element-ui/packages/option/Option'
 import ElTag from 'element-ui/packages/tag'
 import ElScrollbar from 'element-ui/packages/scrollbar'
 import debounce from 'throttle-debounce/debounce'
-import Clickoutside from 'element-ui/src/utils/clickoutside'
+import Clickoutside from 'element-ui/src/directives/clickoutside'
 import {
   addResizeListener,
   removeResizeListener
