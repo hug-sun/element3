9c3c33443b31a5f3ca53198b94c90e7ee109e688
haocaixia <15810707091@163.com>
2020-11-18 16:14:01 +0800

    fix: update element3.d.ts and test

--- types/element3.d.ts
+++ types/element3.d.ts
@@ -49,46 +49,41 @@ export { ElUpload } from './upload'
 export { ElTabs } from './tabs'
 export { ElPageHeader } from './page-header'
 export { ElDropdown } from './dropdown'
-
-import { ElCollapse } from './collapse'
-import { ElCollapseItem } from './collapse-item'
-import { ElColorPicker } from './color-picker'
-import { ElDatePicker } from './date-picker'
-import { ElDialog } from './dialog'
-
-import { ElDropdownItem } from './dropdown-item'
-import { ElDropdownMenu } from './dropdown-menu'
-import { ElForm } from './form'
-import { ElFormItem } from './form-item'
-import { ElInput } from './input'
-import { ElLoading } from './loading'
-import { ElMenu } from './menu'
-import { ElMenuItem } from './menu-item'
-import { ElMenuItemGroup } from './menu-item-group'
-import { ElNotification } from './notification'
-import { ElOption } from './option'
-import { ElOptionGroup } from './option-group'
-import { ElPopover } from './popover'
-import { ElStep } from './step'
-import { ElSubmenu } from './submenu'
-import { ElTable } from './table'
-import { ElTableColumn } from './table-column'
-import { ElTabPane } from './tab-pane'
-import { ElTimeline } from './timeline'
-import { ElTimelineItem } from './timeline-item'
-import { ElTimePicker } from './time-picker'
-import { ElTimeSelect } from './time-select'
-import { ElTooltip } from './tooltip'
-import { ElTransfer } from './transfer'
-import { ElTree, TreeData } from './tree'
-import { ElDivider } from './divider'
-import { ElIcon } from './icon'
-import { ElCalendar } from './calendar'
-import { ElImage } from './image'
-import { ElBacktop } from './backtop'
-import { ElInfiniteScroll } from './infinite-scroll'
-import { ElDrawer } from './drawer'
-import { ElPopconfirm } from './popconfirm'
+export { ElCollapse } from './collapse'
+export { ElCollapseItem } from './collapse-item'
+export { ElColorPicker } from './color-picker'
+export { ElDatePicker } from './date-picker'
+export { ElDialog } from './dialog'
+export { ElDropdownItem } from './dropdown-item'
+export { ElDropdownMenu } from './dropdown-menu'
+export { ElForm } from './form'
+export { ElFormItem } from './form-item'
+export { ElMenu } from './menu'
+export { ElMenuItem } from './menu-item'
+export { ElMenuItemGroup } from './menu-item-group'
+export { ElOption } from './option'
+export { ElOptionGroup } from './option-group'
+export { ElPopover } from './popover'
+export { ElStep } from './step'
+export { ElSubmenu } from './submenu'
+export { ElTable } from './table'
+export { ElTableColumn } from './table-column'
+export { ElTabPane } from './tab-pane'
+export { ElTimeline } from './timeline'
+export { ElTimelineItem } from './timeline-item'
+export { ElTimePicker } from './time-picker'
+export  { ElTimeSelect } from './time-select'
+export { ElTooltip } from './tooltip'
+export { ElTransfer } from './transfer'
+export { ElDivider } from './divider'
+export { ElIcon } from './icon'
+export { ElCalendar } from './calendar'
+export { ElImage } from './image'
+export { ElBacktop } from './backtop'
+export { ElInfiniteScroll } from './infinite-scroll'
+export { ElDrawer } from './drawer'
+export { ElTree, TreeData } from './tree'
+export {ElPopconfirm} from './popconfirm'
 
 export interface InstallationOptions {
   locale: any
@@ -114,111 +109,3 @@ export type ComponentSize = ElementUIComponentSize
 
 /** Horizontal alignment */
 export type HorizontalAlignment = ElementUIHorizontalAlignment
-
-/** Show animation while loading data */
-export const Loading: ElLoading
-
-/** Displays a global notification message at the upper right corner of the page */
-export const Notification: ElNotification
-
-/** Collapse Component */
-export class Collapse extends ElCollapse {}
-
-/** Collapse Item Component */
-export class CollapseItem extends ElCollapseItem {}
-
-/** Date Picker Component */
-export class DatePicker extends ElDatePicker {}
-
-/** Dialog Component */
-export class Dialog extends ElDialog {}
-
-/** Dropdown Item Component */
-export class DropdownItem extends ElDropdownItem {}
-
-/** Dropdown Menu Component */
-export class DropdownMenu extends ElDropdownMenu {}
-
-/** Form Component */
-export class Form extends ElForm {}
-
-/** Form Item Component */
-export class FormItem extends ElFormItem {}
-
-/** Input Component */
-export class Input extends ElInput {}
-
-/** Menu that provides navigation for your website */
-export class Menu extends ElMenu {}
-
-/** Menu Item Component */
-export class MenuItem extends ElMenuItem {}
-
-/** Menu Item Group Component */
-export class MenuItemGroup extends ElMenuItemGroup {}
-
-/** Dropdown Select Option Component */
-export class Option extends ElOption {}
-
-/** Dropdown Select Option Group Component */
-export class OptionGroup extends ElOptionGroup {}
-
-/** Popover Component */
-export class Popover extends ElPopover {}
-
-/** Step Component */
-export class Step extends ElStep {}
-
-/** Submenu Component */
-export class Submenu extends ElSubmenu {}
-
-/** Table Component */
-export class Table extends ElTable {}
-
-/** Table Column Component */
-export class TableColumn extends ElTableColumn {}
-
-/** Tab Pane Component */
-export class TabPane extends ElTabPane {}
-
-/** Timeline Component */
-export class Timeline extends ElTimeline {}
-
-/** Timeline Item Component */
-export class TimelineItem extends ElTimelineItem {}
-
-/** TimePicker Component */
-export class TimePicker extends ElTimePicker {}
-
-/** TimeSelect Component */
-export class TimeSelect extends ElTimeSelect {}
-
-/** Tooltip Component */
-export class Tooltip extends ElTooltip {}
-
-/** Tree Component */
-export class Tree<K = any, D = TreeData> extends ElTree<K, D> {}
-
-/** Divider Component */
-export class Divider extends ElDivider {}
-
-/** Image Component */
-export class Image extends ElImage {}
-
-/** Icon Component */
-export class Icon extends ElIcon {}
-
-/** Calendar Component */
-export class Calendar extends ElCalendar {}
-
-/** Backtop Component */
-export class Backtop extends ElBacktop {}
-
-/** InfiniteScroll Directive */
-export const InfiniteScroll: PluginObject<ElInfiniteScroll>
-
-/** Drawer Component */
-export class Drawer extends ElDrawer {}
-
-/** Popconfirm Component */
-export class Popconfirm extends ElPopconfirm {}
