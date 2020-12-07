2a0bc80ccd9a485ab29532c9fc4a60827e3fe159
leo <1151215064@qq.com>
2020-12-07 15:46:23 +0800

    feat: Refactor Link component

--- packages/link/__tests__/Link.spec.js
+++ packages/link/__tests__/Link.spec.js
@@ -2,55 +2,81 @@ import { mount } from '@vue/test-utils'
 import Link from '../Link.vue'
 describe('Link', () => {
   describe('props', () => {
-    it('type', () => {
+    it('initialize the Link component', () => {
+      const wrapper = mount(Link)
+      expect(wrapper.find('i').exists()).toBe(false)
+      expect(wrapper.find('span').exists()).toBe(false)
+    })
+    it('should show content', () => {
+      const content = 'Link'
       const wrapper = mount(Link, {
-        props: {
-          type: 'primary'
+        slots: {
+          default: content
         }
       })
-
-      expect(wrapper.classes()).toContain('el-link--primary')
+      expect(wrapper.text()).toContain('Link')
     })
 
-    it('icon', () => {
+    it('set the type, link displays the corresponding style', () => {
+      const type = 'primary'
       const wrapper = mount(Link, {
         props: {
-          icon: 'el-icon-search'
+          type
         }
       })
-
-      expect(wrapper.find('.el-icon-search').exists()).toBe(true)
+      expect(wrapper.classes()).toContain(`el-link--${type}`)
     })
 
-    it('href', () => {
+    it('set the disabled, link displays the corresponding style', () => {
+      const disabled = true
       const wrapper = mount(Link, {
         props: {
-          href: 'https://element3-ui.com/'
+          disabled
         }
       })
+      expect(wrapper.classes()).toContain('is-disabled')
+    })
 
-      expect(wrapper.attributes('href')).toBe('https://element3-ui.com/')
+    it('set the underline, link displays the corresponding style', () => {
+      const underline = true
+      const wrapper = mount(Link, {
+        props: {
+          underline
+        }
+      })
+      console.log(wrapper.vm)
+      expect(wrapper.classes()).toContain('is-underline')
+    })
+
+    it('set the href', () => {
+      const href = 'https://element3-ui.com/'
+      const wrapper = mount(Link, {
+        props: {
+          href
+        }
+      })
+      expect(wrapper.attributes('href')).toBe(href)
+    })
+
+    it('set the icon, link displays the corresponding style', () => {
+      const icon = 'el-icon-search'
+      const wrapper = mount(Link, {
+        props: {
+          icon
+        }
+      })
+      const i = wrapper.find('i')
+      expect(i.exists()).toBe(true)
+      expect(i.classes()).toContain(icon)
     })
 
     it('should get target attr value', () => {
-      // because set v-bind='$attrs' to a element
       const wrapper = mount(Link, {
         props: {
           target: '_blank'
         }
       })
-
       expect(wrapper.attributes('target')).toBe('_blank')
-    })
-
-    it('should link is disabled when set disabled prop to be true', () => {
-      const wrapper = mount(Link, {
-        props: {
-          disabled: true
-        }
-      })
-
-      expect(wrapper.classes()).toContain('is-disabled')
     })
   })
 
@@ -58,12 +84,11 @@ describe('Link', () => {
     it('should captured click events emitted via click', () => {
       const wrapper = mount(Link)
       wrapper.trigger('click')
-
       expect(wrapper.emitted('click')).toBeTruthy()
     })
 
     describe("can't captured click event emitted", () => {
-      it('when disabled prop is equal to true ', () => {
+      it('when disabled prop is equal to true', () => {
         const wrapper = mount(Link, {
           props: {
             disabled: true
@@ -73,10 +98,10 @@ describe('Link', () => {
         expect(wrapper.emitted('click')).toBeFalsy()
       })
 
-      it('when href prop is to be truthy ', () => {
+      it('when href prop is to be truthy', () => {
         const wrapper = mount(Link, {
           props: {
-            href: 'http://www.baidu.com'
+            href: 'https://element3-ui.com/'
           }
         })
         wrapper.trigger('click')
