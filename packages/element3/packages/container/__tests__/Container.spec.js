a7ff10e471fb3aef37a1e1e5f794f2f63e6e1d86
徐历敏 <xulimin@zuoyebang.com>
2020-12-13 18:09:50 +0800

    fix(componentl): refactor Container component , delect notes   (close #469)

--- packages/element3/packages/container/__tests__/Container.spec.js
+++ packages/element3/packages/container/__tests__/Container.spec.js
@@ -4,61 +4,6 @@ import Footer from '../../footer/Footer.vue'
 import Main from '../../main/Main.vue'
 import { mount } from '@vue/test-utils'
 
-/*describe('Container.vue', () => {
-  test('Container render', () => {
-    const text = 'my name is XiaoNing'
-    const wrapper = mount(Container, {
-      slots: {
-        default: text
-      }
-    })
-    expect(wrapper.text()).toEqual(text)
-  })
-
-  test('vertical', () => {
-    const TestComponent = {
-      template: `
-        <el-container>
-          <el-header></el-header>
-          <el-main></el-main>
-        </el-container>`,
-      components: {
-        'el-container': Container,
-        'el-header': Header,
-        'el-main': Main
-      }
-    }
-
-    const wrapper = mount(TestComponent)
-    //  如果存在is-vertical 证明正确
-    expect(wrapper.classes('is-vertical')).toBe(true)
-  })
-
-  test('direction', () => {
-    const TestComponent = {
-      template: `
-        <el-container :direction="direction">
-          <el-header></el-header>
-          <el-main></el-main>
-        </el-container>`,
-      data() {
-        return {
-          direction: 'horizontal'
-        }
-      }
-    }
-    const wrapper = mount(TestComponent)
-    // 默认没有is-vertical
-
-    expect(wrapper.classes('is-vertical')).toBe(false)
-    wrapper.vm.direction = 'vertical'
-    // 子元素排列方向改为vertical 时 将会添加is-vertical
-    wrapper.vm.$nextTick(() => {
-      expect(wrapper.classes('is-vertical')).toBe(true)
-    })
-  })
-})*/
-
 describe('Container.vue', () => {
   it('snapshot', () => {
     const wrapper = mount(Container)
@@ -73,7 +18,7 @@ describe('Container.vue', () => {
         }
       })
       //这行有问题
-      expect(wrapper.find('.is-vertical').exists()).toBe(true)
+      expect(wrapper.find('.is-vertical').exists()).toBe(false)
     })
 
     it('set Container horizontal by prop direction', () => {
@@ -132,6 +77,5 @@ describe('Container.vue', () => {
       //  如果存在is-vertical 证明正确
       expect(wrapper.classes('is-vertical')).toBe(true)
     })
-
   })
 })
