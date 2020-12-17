7501526369bd1101e861e43d06c0f3f2a580efb7
uuoocckk-983 <799754081@qq.com>
2020-12-17 19:37:25 +0800

    refactor(rate): rate component

--- packages/element3/packages/rate/Rate.vue
+++ packages/element3/packages/rate/Rate.vue
@@ -1,41 +1,37 @@
 <template>
   <div
     class="el-rate"
-    @keydown="handleKey"
     role="slider"
     :aria-valuenow="modelValue"
-    :aria-valuetext="text"
+    :aria-valuetext="1"
     aria-valuemin="0"
     :aria-valuemax="max"
     tabindex="0"
+    @keydown.stop.prevent="pressKeyUpdateRate($event)"
   >
     <span
       v-for="(item, key) in max"
       class="el-rate__item"
-      @mousemove="setCurrentValue(item, $event)"
-      @mouseleave="resetCurrentValue"
-      @click="selectValue(item)"
-      :style="{ cursor: rateDisabled ? 'auto' : 'pointer' }"
       :key="key"
+      :style="{ cursor: rateDisabled ? 'auto' : 'pointer'}"
+      @mousemove="slideSelectRate(item)"
+      @click="clickUpdateRate(item)"
     >
       <i
         class="el-rate__icon"
-        :class="[classes[item - 1], { hover: hoverIndex === item }]"
-        :style="getIconStyle(item)"
+        :class="[{hover: true}, classes[item-1]]"
       >
         <i
           class="el-rate__decimal"
-          v-if="showDecimalIcon(item)"
-          :class="decimalIconClass"
-          :style="decimalStyle"
+          v-if="allowHalf"
         ></i>
       </i>
     </span>
     <span
       v-if="showText || showScore"
       class="el-rate__text"
-      :style="{ color: textColor }"
-      >{{ text }}</span
+      >
+      </span
     >
   </div>
 </template>
@@ -51,7 +47,6 @@ import {
   getCurrentInstance
 } from 'vue'
 import { hasClass } from '../../src/utils/dom'
-import { migrating } from '../../src/use/migrating'
 
 export default {
   name: 'ElRate',
@@ -154,346 +149,88 @@ export default {
       scoreTemplate,
       showText
     } = toRefs(props)
+   
+    const rateDisabled = () => {
+      return disabled
+    }
+    const unifiediconClasses = () => {//后续要加入color，需要改名和进一步封装
+    const unrefIconClasses = unref(iconClasses)
+      return Array.isArray(unrefIconClasses) ? {
+          [lowThreshold.value]: unrefIconClasses[0],
+          [highThreshold.value]: {value: unrefIconClasses[1], excluded: true},
+          [max.value]: unrefIconClasses[2]
+        } : unrefIconClasses.value
+    }
+    //完善各函数后，加上有关disabled判断
+    const matchClassOrColor = (() => {
+      //需要把数组统一转成对象 --> unifiediconClasses
+      //拿到对象，判断分数在某段中的包含关系，可以确定临界值属于哪个分段exclude：false是包含，exclude：true是不包含   
+      //当前选中的值在哪个分段中，就把相应的class return出去
+      
+      const matchKey = Math.max(Object.keys(unifiediconClasses()).filter(+key=>{
+        const excluded = unifiediconClasses()[key] instanceof Object ? unifiediconClasses()[key].encluded : false
+        return excluded ? modelValue.value < key : modelValue.value <= key
+      }))
+      console.log(175,Object.keys(unifiediconClasses()).filter(+key=>{
+        const excluded = unifiediconClasses()[key] instanceof Object ? unifiediconClasses()[key].encluded : false
+        return excluded ? modelValue.value < key : modelValue.value <= key
+      }))
 
-    migrating({
-      'text-template': 'text-template is renamed to score-template.'
+      //matchKey对应的value可能是对象，可能是字符串，对象要再取value
+      console.log(matchKey);
+      return iconClasses[matchKey] instanceof Object ? iconClasses[matchKey].value : iconClasses[matchKey]
     })
-
-    modelValue || emit('update:modelValue', 0)
-
-    const elForm = inject('elForm', {})
-
-    const rateDisabled = useDisabled(disabled, elForm)
-
-    const {
-      currentValue,
-      hoverIndex,
-      pointerAtLeftHalf,
-      setCurrentValue,
-      resetCurrentValue,
-      selectValue,
-      handleKey
-    } = useCurrentValue({
-      modelValue,
-      allowHalf,
-      rateDisabled,
-      max
+    const activeClass = computed(() => {
+      //依据当前选中的值，得到从iconclass（数组或对象）中相应的class --> matchClassOrColor
+      matchClassOrColor(modelValue,iconClasses)
+      console.log('class计算属性');
     })
-
-    const { classMap, colorMap } = useMaps({
-      colors,
-      iconClasses,
-      lowThreshold,
-      highThreshold,
-      max
-    })
-
-    const { classes, decimalIconClass } = useClasses({
-      currentValue,
-      modelValue,
-      classMap,
-      allowHalf,
-      max,
-      rateDisabled,
-      disabledVoidIconClass,
-      voidIconClass
-    })
-
-    const { activeColor, getIconStyle } = useColor({
-      currentValue,
-      colorMap,
-      rateDisabled,
-      disabledVoidColor,
-      voidColorProp: voidColor
-    })
-
-    const { decimalStyle, showDecimalIcon } = useDecimal({
-      currentValue,
-      modelValue,
-      allowHalf,
-      rateDisabled,
-      pointerAtLeftHalf,
-      activeColor
-    })
-
-    const text = computed(() => {
-      let result = ''
-      if (showScore.value) {
-        result = scoreTemplate.value.replace(
-          /\{\s*value\s*\}/,
-          unref(rateDisabled) ? modelValue.value : currentValue.value
-        )
-      } else if (showText.value) {
-        result = texts.value[Math.ceil(currentValue.value) - 1]
+    const classes = computed(() => {
+      const result = [];
+      let i = 0;
+      let criticalPoint = modelValue.value
+      for (; i < criticalPoint; i++){
+        console.log(matchClassOrColor());
+        result.push(matchClassOrColor()) //我想得到选中效果的icon的class --> activeClass
       }
+      for (; i < max.value; i++){
+        result.push('el-icon-star-off')
+      }
+      // console.log(result);
       return result
     })
+    const slideSelectRate = (value, event) => {
 
-    return {
-      // state
-      rateDisabled,
-      classMap,
-      classes,
-      text,
-      hoverIndex,
-      decimalStyle,
-      decimalIconClass,
-      // methods
-      getIconStyle,
-      setCurrentValue,
-      resetCurrentValue,
-      selectValue,
-      handleKey,
-      showDecimalIcon
     }
-  }
-}
-
-const getValueFromMap = (value, map) => {
-  map = unref(map)
-  const matchedKeys = Object.keys(map)
-    .filter((key) => {
-      const val = map[key]
-      const excluded = val instanceof Object ? val.excluded : false
-      return excluded ? value < key : value <= key
-    })
-    .sort((a, b) => a - b)
-  const matchedValue = map[matchedKeys[0]]
-  return matchedValue instanceof Object
-    ? matchedValue.value
-    : matchedValue || ''
-}
-
-const useDisabled = (disabled, elForm) => {
-  return computed(() => disabled.value || elForm.disabled)
-}
-
-const useMaps = ({ colors, iconClasses, lowThreshold, highThreshold, max }) => {
-  const useMap = (mapData, { lowThreshold, highThreshold, max }) => {
-    return computed(() =>
-      Array.isArray(mapData)
-        ? {
-            [lowThreshold.value]: mapData[0],
-            [highThreshold.value]: { value: mapData[1], excluded: true },
-            [max.value]: mapData[2]
-          }
-        : mapData
-    )
-  }
-  return {
-    classMap: useMap(unref(iconClasses), { lowThreshold, highThreshold, max }),
-    colorMap: useMap(unref(colors), { lowThreshold, highThreshold, max })
-  }
-}
-
-const useClasses = ({
-  currentValue,
-  modelValue,
-  classMap,
-  allowHalf,
-  max,
-  rateDisabled,
-  disabledVoidIconClass,
-  voidIconClass
-}) => {
-  const activeClass = computed(() =>
-    getValueFromMap(currentValue.value, unref(classMap))
-  )
-  const voidClass = computed(() =>
-    unref(rateDisabled) ? disabledVoidIconClass.value : voidIconClass.value
-  )
-  const decimalIconClass = computed(() =>
-    getValueFromMap(modelValue.value, unref(classMap))
-  )
-
-  const classes = computed(() => {
-    const result = []
-    let i = 0
-    let threshold = currentValue.value
-    if (
-      allowHalf.value &&
-      currentValue.value !== Math.floor(currentValue.value)
-    ) {
-      threshold--
+    const clickUpdateRate = (value) => {
+      setRate(value)
     }
-    for (; i < threshold; i++) {
-      result.push(activeClass.value)
-    }
-    for (; i < max.value; i++) {
-      result.push(voidClass.value)
-    }
-    return result
-  })
-
-  return {
-    classes,
-    decimalIconClass
-  }
-}
-
-const useColor = ({
-  currentValue,
-  colorMap,
-  rateDisabled,
-  disabledVoidColor,
-  voidColorProp
-}) => {
-  const activeColor = computed(() =>
-    getValueFromMap(currentValue.value, colorMap)
-  )
-  const getIconStyle = (item) => {
-    const voidColor = rateDisabled.value
-      ? disabledVoidColor.value
-      : voidColorProp.value
-    return {
-      color: unref(item <= currentValue.value ? activeColor.value : voidColor)
-    }
-  }
-  return {
-    activeColor,
-    getIconStyle
-  }
-}
-
-const useDecimal = ({
-  currentValue,
-  modelValue,
-  allowHalf,
-  rateDisabled,
-  pointerAtLeftHalf,
-  activeColor
-}) => {
-  const valueDecimal = computed(
-    () => modelValue.value * 100 - Math.floor(modelValue.value) * 100
-  )
-  const decimalStyle = computed(() => {
-    let width = ''
-    if (unref(rateDisabled)) {
-      width = `${valueDecimal.value}%`
-    } else if (allowHalf.value) {
-      width = '50%'
-    }
-    return {
-      color: activeColor.value,
-      width
-    }
-  })
-
-  const showDecimalIcon = (item) => {
-    const showWhenDisabled =
-      unref(rateDisabled) &&
-      valueDecimal.value > 0 &&
-      item - 1 < modelValue.value &&
-      item > modelValue.value
-    /* istanbul ignore next */
-    const showWhenAllowHalf =
-      allowHalf.value &&
-      pointerAtLeftHalf &&
-      item - 0.5 <= currentValue.value &&
-      item > currentValue.value
-    return showWhenDisabled || showWhenAllowHalf
-  }
-
-  return {
-    decimalStyle,
-    showDecimalIcon
-  }
-}
-
-const useCurrentValue = ({ modelValue, allowHalf, rateDisabled, max }) => {
-  const { emit } = getCurrentInstance()
-  const currentValue = ref(modelValue.value)
-  const pointerAtLeftHalf = ref(false)
-  const hoverIndex = ref(-1)
-  watch(modelValue, (v) => {
-    currentValue.value = v
-  })
-
-  const setCurrentValue = (value, event) => {
-    if (rateDisabled.value) {
-      return
-    }
-    /* istanbul ignore if */
-    if (allowHalf.value) {
-      let target = event.target
-      if (hasClass(target, 'el-rate__item')) {
-        target = target.querySelector('.el-rate__icon')
+    const pressKeyUpdateRate = (e) => {
+      let value = modelValue.value
+      //增加和减少需要判断临界值
+      if(e.keyCode === 38 || e.keyCode === 39){
+        value += 1
       }
-      if (hasClass(target, 'el-rate__decimal')) {
-        target = target.parentNode
+      if(e.keyCode === 37 || e.keyCode === 40){
+        value -= 1
       }
-      pointerAtLeftHalf.value = event.offsetX * 2 <= target.clientWidth
-      currentValue.value = pointerAtLeftHalf.value ? value - 0.5 : value
-    } else {
-      currentValue.value = value
+      
+      setRate(value)
     }
-    hoverIndex.value = value
-  }
-
-  const resetCurrentValue = () => {
-    if (rateDisabled.value) {
-      return
-    }
-    if (allowHalf.value) {
-      pointerAtLeftHalf.value =
-        modelValue.value !== Math.floor(modelValue.value)
-    }
-    currentValue.value = modelValue.value
-    hoverIndex.value = -1
-  }
-
-  const selectValue = (value) => {
-    if (rateDisabled.value) {
-      return
-    }
-    if (allowHalf.value && pointerAtLeftHalf.value) {
-      emit('update:modelValue', currentValue.value)
-      emit('change', currentValue.value)
-    } else {
+    const setRate = (value) => {
       emit('update:modelValue', value)
       emit('change', value)
     }
-  }
-
-  const handleKey = (e) => {
-    if (rateDisabled.value) {
-      return
+    
+    return {
+      // state
+      rateDisabled,
+      classes,
+      // methods
+      clickUpdateRate,
+      slideSelectRate,
+      pressKeyUpdateRate
     }
-    let value = currentValue.value
-    const keyCode = e.keyCode
-    if (keyCode === 38 || keyCode === 39) {
-      // left / down
-      if (allowHalf.value) {
-        value += 0.5
-      } else {
-        value += 1
-      }
-      e.stopPropagation()
-      e.preventDefault()
-    } else if (keyCode === 37 || keyCode === 40) {
-      if (allowHalf.value) {
-        value -= 0.5
-      } else {
-        value -= 1
-      }
-      e.stopPropagation()
-      e.preventDefault()
-    }
-    value = value < 0 ? 0 : value
-    value = value > max.value ? max.value : value
-
-    emit('update:modelValue', value)
-    emit('change', value)
-  }
-
-  return {
-    currentValue,
-    hoverIndex,
-    pointerAtLeftHalf,
-    setCurrentValue,
-    resetCurrentValue,
-    selectValue,
-    handleKey
   }
 }
 </script>
