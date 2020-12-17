d94c756627dbaca74086c01cf2094ecb7bb1fb35
Simon Jun Sheng Liu <wind13@163.com>
2020-12-17 18:07:34 +0800

    fix(component): #501 Done percentage props change

--- packages/element3/packages/progress/__tests__/Progress.spec.js
+++ packages/element3/packages/progress/__tests__/Progress.spec.js
@@ -1,12 +1,12 @@
 import { mount } from '@vue/test-utils'
 import Progress from '../Progress.vue'
+// import { getByTestId } from '@testing-library/jest-dom'
 
 describe('Progress.vue', () => {
   it('should create default Progress component and HTML structure', () => {
+    const percentage = 55
     const wrapper = mount(Progress, {
-      props: {
-        percentage: 55
-      }
+      props: { percentage }
     })
     hasClass(wrapper, 'el-progress')
     hasClass(wrapper, 'el-progress--line')
@@ -16,12 +16,36 @@ describe('Progress.vue', () => {
     existsElem(wrapper, '.el-progress-bar > .el-progress-bar__outer')
     existsElem(wrapper, '.el-progress-bar__outer .el-progress-bar__inner')
 
-    containText(wrapper, '.el-progress__text', '55%')
-    expect(wrapper.find('.el-progress-bar__inner').attributes().style).toBe(
-      'width: 55%;'
-    )
+    testPercentage(wrapper, percentage)
+
+    // expect(getByTestId('temptest')).not.toBeEmptyDOMElement()
+    expect(wrapper.find('.temptest').text()).toBe('')
+  })
+
+  describe('props', () => {
+    it('percentage', async () => {
+      const wrapper = mount(Progress, {
+        props: {
+          percentage: 58
+        }
+      })
+      await wrapper.setProps({ percentage: 28 })
+      testPercentage(wrapper, 28)
+    })
   })
 })
+
+function testPercentage(wrapper, percentage) {
+  containText(wrapper, '.el-progress__text', `${percentage}%`)
+  containStyle(wrapper, '.el-progress-bar__inner', `width: ${percentage}%;`)
+}
+
+function containStyle(wrapper, selector, strStyle) {
+  const elem = wrapper.find(selector)
+  expect(elem.attributes().style).toBeDefined()
+  expect(elem.attributes().style).toContain(strStyle)
+}
+
 function containText(wrapper, selector, text) {
   expect(wrapper.find(selector).text()).toContain(text)
 }
