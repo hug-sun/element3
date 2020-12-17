bb7c0afdfcd1f65af7276adcdc4b3254415ee9d2
Simon Jun Sheng Liu <wind13@163.com>
2020-12-17 15:30:02 +0800

    fix(component): #501 Create HTML structure

--- packages/element3/packages/progress/Progress.vue
+++ packages/element3/packages/progress/Progress.vue
@@ -1,5 +1,12 @@
 <template>
-  <div>55%</div>
+  <div class="el-progress el-progress--line">
+    <div class="el-progress-bar">
+      <div class="el-progress-bar__outer">
+        <div class="el-progress-bar__inner" style="width: 55%"></div>
+      </div>
+    </div>
+    <div class="el-progress__text">55%</div>
+  </div>
 </template>
 
 <script>
--- packages/element3/packages/progress/__tests__/Progress.spec.js
+++ packages/element3/packages/progress/__tests__/Progress.spec.js
@@ -2,12 +2,34 @@ import { mount } from '@vue/test-utils'
 import Progress from '../Progress.vue'
 
 describe('Progress.vue', () => {
-  it('should create progress component', () => {
+  it('should create default Progress component and HTML structure', () => {
     const wrapper = mount(Progress, {
       props: {
         percentage: 55
       }
     })
-    expect(wrapper.text()).toContain('55%')
+    hasClass(wrapper, 'el-progress')
+    hasClass(wrapper, 'el-progress--line')
+
+    existsElem(wrapper, '.el-progress > .el-progress-bar')
+    existsElem(wrapper, '.el-progress > .el-progress__text')
+    existsElem(wrapper, '.el-progress-bar > .el-progress-bar__outer')
+    existsElem(wrapper, '.el-progress-bar__outer .el-progress-bar__inner')
+
+    containText(wrapper, '.el-progress__text', '55%')
+    expect(wrapper.find('.el-progress-bar__inner').attributes().style).toBe(
+      'width: 55%;'
+    )
   })
 })
+function containText(wrapper, selector, text) {
+  expect(wrapper.find(selector).text()).toContain(text)
+}
+
+function hasClass(elem, className) {
+  expect(elem.classes().includes(className)).toBeTruthy()
+}
+
+function existsElem(wrapper, selector) {
+  expect(wrapper.find(selector).exists()).toBeTruthy()
+}
