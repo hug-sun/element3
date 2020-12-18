0656a3d2d69f8e549320c49d2722506ce1d44a56
leo <1151215064@qq.com>
2020-12-19 01:01:03 +0800

    chore(Notification): add props.js

--- packages/element3/packages/notification/Notification.vue
+++ packages/element3/packages/notification/Notification.vue
@@ -39,28 +39,10 @@
 
 <script>
 import { computed, getCurrentInstance, ref } from 'vue'
+import { notificationProps } from './props.js'
 export default {
   name: 'ElNotification',
-  props: {
-    customClass: { type: String, default: '' },
-    dangerouslyUseHTMLString: { type: Boolean, default: false },
-    duration: { type: Number, default: 4500 },
-    iconClass: { type: String, default: '' },
-    id: { type: String, default: '' },
-    verticalOffset: { type: Number, default: 0 },
-    message: [String, Object],
-    position: { type: String, default: 'top-right' },
-    onClick: null,
-    showClose: { type: Boolean, default: true },
-    title: { type: String, default: '' },
-    type: {
-      type: String,
-      default: '',
-      validator(val) {
-        return ['', 'success', 'warning', 'info', 'error'].includes(val)
-      }
-    }
-  },
+  props: notificationProps,
   emits: ['close'],
   setup(props, { emit }) {
     const instance = getCurrentInstance()
