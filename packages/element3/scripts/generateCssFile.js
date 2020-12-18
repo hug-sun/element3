d3f54eded927371bdd063ab5f3d839fe3b574567
cuixiaorui <cui_xiaorui@126.com>
2020-12-18 17:03:15 +0800

    refactor(element3): generate css file

--- packages/element3/scripts/generateCssFile.js
+++ packages/element3/scripts/generateCssFile.js
@@ -1,35 +1,34 @@
-var fs = require('fs')
-var path = require('path')
-var Components = require('../components.json')
-var themes = ['theme-chalk']
-Components = Object.keys(Components)
-var basepath = path.resolve(__dirname, '../packages/')
+const fs = require('fs')
+const path = require('path')
+const blacklist = ['icon', 'option', 'option-group', 'theme-chalk']
+const componentPath = path.resolve(__dirname, '../packages')
+const themePath = path.resolve(componentPath, 'theme-chalk/src')
+const fileSuffix = '.scss'
+const indexFileName = 'index' + fileSuffix
+const outputIndexFilePath = path.resolve(themePath, indexFileName)
 
-function fileExists(filePath) {
-  try {
-    return fs.statSync(filePath).isFile()
-  } catch (err) {
-    return false
-  }
+;(function main() {
+  save(genCssCode())
+})()
+
+function genCssCode() {
+  var indexContent = '@import "./base.scss";\n'
+  return getComponentNameList().reduce((context, filePath) => {
+    return (context += '@import "./' + filePath + '";\n')
+  }, indexContent)
 }
 
-themes.forEach((theme) => {
-  var isSCSS = theme !== 'theme-default'
-  var indexContent = isSCSS
-    ? '@import "./base.scss";\n'
-    : '@import "./base.css";\n'
-  Components.forEach(function (key) {
-    if (['icon', 'option', 'option-group'].indexOf(key) > -1) return
-    var fileName = key + (isSCSS ? '.scss' : '.css')
-    indexContent += '@import "./' + fileName + '";\n'
-    var filePath = path.resolve(basepath, theme, 'src', fileName)
-    if (!fileExists(filePath)) {
-      fs.writeFileSync(filePath, '', 'utf8')
-      console.log(theme, ' 创建遗漏的 ', fileName, ' 文件')
-    }
-  })
-  fs.writeFileSync(
-    path.resolve(basepath, theme, 'src', isSCSS ? 'index.scss' : 'index.css'),
-    indexContent
-  )
-})
+function getComponentNameList() {
+  return fs
+    .readdirSync(componentPath)
+    .filter((name) => {
+      return !blacklist.includes(name)
+    })
+    .map((componentName) => {
+      return componentName + fileSuffix
+    })
+}
+
+function save(cssCode) {
+  fs.writeFileSync(outputIndexFilePath, cssCode)
+}
