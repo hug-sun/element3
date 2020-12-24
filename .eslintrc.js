b54387aaf4170ebf9c6848bb61b4de03fee1963a
cuixiaorui <cui_xiaorui@126.com>
2020-12-24 15:51:35 +0800

    style: fix lint error

--- .eslintrc.js
+++ .eslintrc.js
@@ -18,6 +18,8 @@ module.exports = {
   },
   rules: {
     'no-console': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
-    'no-debugger': process.env.NODE_ENV === 'production' ? 'warn' : 'off'
+    'no-debugger': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
+    '@typescript-eslint/no-var-requires': 'off',
+    '@typescript-eslint/no-empty-function': 'off'
   }
 }
