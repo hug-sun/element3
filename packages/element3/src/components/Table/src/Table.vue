dc836370bf47493cab739d19205a0866403a59fe
cuixiaorui <cui_xiaorui@126.com>
2021-01-26 15:49:54 +0800

    feat(element3): Table add Basis function

--- packages/element3/src/components/Table/src/Table.vue
+++ packages/element3/src/components/Table/src/Table.vue
@@ -2,7 +2,7 @@
   <table>
     <thead>
       <tr>
-        <template v-for="(column, index) in columns" :key="index">
+        <template v-for="(column, index) in tableHeads" :key="index">
           <th>{{ column.label }}</th>
         </template>
       </tr>
@@ -14,43 +14,86 @@
         </tr>
       </template>
     </tbody>
+    <slot></slot>
   </table>
 </template>
-
 <script>
-import { computed, defineComponent, unref } from 'vue'
+import {
+  computed,
+  defineComponent,
+  ref,
+  provide,
+  getCurrentInstance,
+  toRefs
+} from 'vue'
 export default defineComponent({
   name: 'ElNewTable',
   props: ['data'],
-  setup(props, { slots }) {
-    const columns = computed(() => {
-      const tableColumns = slots.default()
+  setup(props) {
+    const { data } = toRefs(props)
 
-      return tableColumns.map((columnsVnode) => {
-        const { prop, label } = columnsVnode.props
-        return {
-          prop,
-          label
-        }
-      })
-    })
+    useProvide()
 
-    function createRows(columns) {
-      return computed(() => {
-        return props.data.map((item) => {
-          return columns.reduce((result, { prop: key }) => {
-            result[key] = item[key]
-            return result
-          }, {})
-        })
-      })
+    const { columns, registryColumn } = useColumns()
+    const { tableHeads } = useTableHeads(columns)
+    const { rows } = useRows(data, tableHeads)
+
+    return {
+      registryColumn,
+      tableHeads,
+      rows
     }
-
-    const rows = createRows(unref(columns))
-
-    return { columns, rows }
   }
 })
+
+function useProvide() {
+  const instance = getCurrentInstance()
+
+  provide('table', instance.proxy)
+}
+
+function useColumns() {
+  const columns = ref([])
+  function registryColumn(column) {
+    columns.value.push(column)
+  }
+
+  return {
+    columns,
+    registryColumn
+  }
+}
+
+function useTableHeads(columns) {
+  const tableHeads = computed(() => {
+    return columns.value.map((columnVM) => {
+      const { prop, label } = columnVM.props
+      return {
+        prop,
+        label
+      }
+    })
+  })
+
+  return {
+    tableHeads
+  }
+}
+
+function useRows(data, tableHeads) {
+  const rows = computed(() => {
+    return data.value.map((item) => {
+      return tableHeads.value.reduce((result, { prop: key }) => {
+        result[key] = item[key]
+        return result
+      }, {})
+    })
+  })
+
+  return {
+    rows
+  }
+}
 </script>
 
 <style></style>
--- packages/element3/src/components/Table/tests/table.spec.js
+++ packages/element3/src/components/Table/tests/table.spec.js
@@ -1,42 +1,87 @@
 import Table from '../src/Table.vue'
 import TableColumn from '../../TableColumn/src/TableColumn.vue'
-import { mount } from '@vue/test-utils'
+import { mount, flushPromises } from '@vue/test-utils'
+import { nextTick } from 'vue'
 
 describe('Table', () => {
-  it('基础表格', () => {
-    const Comp = {
-      template: `
+  describe('基础表格', () => {
+    it('happy path', async () => {
+      const Comp = {
+        template: `
         <el-table
           :data="tableData"
           >
           <el-table-column
             prop="date"
-            label="日期" width="180">
+            label="日期">
           </el-table-column>
         </el-table>
         `,
-      components: {
-        'el-table': Table,
-        'el-table-column': TableColumn
-      },
-      data() {
-        return {
-          tableData: [
-            {
-              date: '2016-05-02',
-              time: 10
-            },
-            {
-              date: '2016-05-03',
-              time: 11
-            }
-          ]
+        components: {
+          'el-table': Table,
+          'el-table-column': TableColumn
+        },
+        data() {
+          return {
+            tableData: [
+              {
+                date: '2016-05-02',
+                time: 10
+              },
+              {
+                date: '2016-05-03',
+                time: 11
+              }
+            ]
+          }
         }
       }
-    }
 
-    const wrapper = mount(Comp)
-    expect(wrapper.text()).toContain('日期')
-    expect(wrapper.text()).toContain('2016-05-02')
+      const wrapper = mount(Comp)
+      await flushPromises()
+
+      expect(wrapper.text()).toContain('日期')
+      expect(wrapper.text()).toContain('2016-05-02')
+    })
+
+    it('table 内存在其他组件', async () => {
+      const Comp = {
+        template: `
+        <el-table
+          :data="tableData"
+          >
+          <el-table-column
+            prop="date"
+            label="日期">
+          </el-table-column>
+          <div></div>
+        </el-table>
+        `,
+        components: {
+          'el-table': Table,
+          'el-table-column': TableColumn
+        },
+        data() {
+          return {
+            tableData: [
+              {
+                date: '2016-05-02',
+                time: 10
+              },
+              {
+                date: '2016-05-03',
+                time: 11
+              }
+            ]
+          }
+        }
+      }
+
+      const wrapper = mount(Comp)
+      await flushPromises()
+
+      expect(wrapper.text()).toContain('日期')
+      expect(wrapper.text()).toContain('2016-05-02')
+    })
   })
 })
