fb68302a6cb18018f63471ac7cb5ee9a69743c67
cuixiaorui <cui_xiaorui@126.com>
2021-01-28 15:18:49 +0800

    refactor: Progress.d.ts rename to types.ts

--- packages/element3/src/components/Progress/src/types.ts
+++ packages/element3/src/components/Progress/src/types.ts
@@ -10,13 +10,15 @@ export type FnColor = (percentage: number) => string
 export type AnyColor = string | Function | unknown[]
 /* eslint-enable*/
 
+export type FormatCallBackFunction = (percentage: number) => void
+
 export interface ProgressProps {
   percentage?: number
   color?: AnyColor
   status?: string
   strokeWidth?: number
   type?: string
-  format?: Function
+  format?: FormatCallBackFunction
   showText?: boolean
   textInside?: boolean
   width?: number
