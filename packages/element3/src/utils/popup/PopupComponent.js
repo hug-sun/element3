cb112c467d807a6521203b74025fade24c78220a
jack.l <i95966@126.com>
2021-01-29 17:33:27 -0800

    refactor-popup: (refactor popup)

---
+++ packages/element3/src/utils/popup/PopupComponent.js
@@ -0,0 +1,60 @@
+import { Teleport, ref, h, Transition, computed, toRefs } from 'vue'
+import { props } from './props'
+import { useZindex, useBodyScroll } from './use'
+
+const popupWrapper = {
+  render({ $slots }) {
+    return <div>{$slots.default ? $slots.default() : null}</div>
+  }
+}
+
+export default {
+  props,
+  emits: ['close'],
+  setup(props, { emit }) {
+    const show = ref(true)
+
+    const zIndex = useZindex()
+
+    useBodyScroll(props)
+
+    const close = () => {
+      if (props.closeOnClickModal) {
+        show.value = false
+        emit('close')
+      }
+    }
+
+    return {
+      zIndex,
+      show,
+      close
+    }
+  },
+  render({ $props, $attrs, $slots, zIndex, close }) {
+    const attrs = computed(() => {
+      return {
+        ...$attrs
+      }
+    })
+    return (
+      <Teleport to={$props.target}>
+        <Transition
+          ref="target"
+          name={$props.transitionClass}
+          onAfterLeave={$props.afterLeaveHandler}
+          appear
+        >
+          <popupWrapper
+            onClick={close}
+            style={{ zIndex }}
+            {...$attrs}
+            v-show={$props.visiable}
+          >
+            {$slots.default ? $slots.default() : null}
+          </popupWrapper>
+        </Transition>
+      </Teleport>
+    )
+  }
+}
---
+++ packages/element3/src/utils/popup/use.js
@@ -0,0 +1,30 @@
+import { computed, onBeforeMount, onBeforeUnmount, onUnmounted } from 'vue'
+import { addClass, removeClass, hasClass } from '../../utils/dom'
+
+let zIndex = 2000
+
+let useBodyScrollCounter = 0
+
+function useZindex() {
+  return computed(() => zIndex++)
+}
+
+function useBodyScroll({ lockScroll }) {
+  const body = document.body
+
+  useBodyScrollCounter++
+  onBeforeMount(() => {
+    if (lockScroll && !hasClass(body, 'el-popup-parent--hidden')) {
+      addClass(body, 'el-popup-parent--hidden')
+    }
+  })
+
+  onUnmounted(() => {
+    useBodyScrollCounter--
+    if (lockScroll || useBodyScrollCounter <= 1) {
+      removeClass(document.body, 'el-popup-parent--hidden')
+    }
+  })
+}
+
+export { useZindex, useBodyScroll }
