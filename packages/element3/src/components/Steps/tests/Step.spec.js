706a96c32d70e4d8d22b2d05642ed5b236fd6754
DivenYoung <DivenYoungth@gmail.com>
2021-02-23 21:05:06 +0800

    feat(Steps): add steps Component

--- packages/element3/src/components/Steps/tests/Step.spec.js
+++ packages/element3/src/components/Steps/tests/Step.spec.js
@@ -1,12 +1,26 @@
 import Step from '../src/Step.vue'
+import Steps from '../src/Steps.vue'
 import { mount } from '@vue/test-utils'
 
 /*
 - [ ] Add a circle
 - [ ] Add a horizontal line
 - [ ] Add a description
- */
+- [ ] x element
+- [ ]
 
+ */
+describe('Steps', () => {
+  it('should have 1 element', function () {
+    let steps = mount(Steps, {
+      slots: {
+        default: [Step, '<div class=".el-step"></div>']
+      }
+    })
+    let step = steps.findAll('.el-step')
+    expect(step.length).toBe(1)
+  })
+})
 describe('Can create a Step component', () => {
   it('should have "Hello" as content', () => {
     const wrapper = mount(Step, {
