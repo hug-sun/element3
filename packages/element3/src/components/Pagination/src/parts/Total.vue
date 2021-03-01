263184fd18efbcb8ed6f8fa3192a4d7465d950b2
WumaCoder <1829913225@qq.com>
2021-03-01 17:21:27 +0800

    refactor(Pagination): add Total Part

---
+++ packages/element3/src/components/Pagination/src/parts/Total.vue
@@ -0,0 +1,12 @@
+<template>
+  <span class="el-pagination__total" v-text="`共 ${pager.total} 条`"></span>
+</template>
+
+<script lang="ts">
+import { Pager } from '../entity/Pager'
+export default {
+  props: {
+    pager: Pager
+  }
+}
+</script>
