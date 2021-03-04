f54b37982b453df462940fd1414c64022a8b7b2d
WumaCoder <1829913225@qq.com>
2021-03-04 10:30:15 +0800

    chore: add play.vue to ignore

--- packages/website/src/play/index.vue
+++ packages/website/src/play/index.vue
@@ -1,28 +1,14 @@
 <template>
-  <el-menu unique-opened>
-    <el-submenu index="2">
-      <template v-slot:title>我的工作台</template>
-      <el-menu-item index="2-1">选项1</el-menu-item>
-    </el-submenu>
-    <el-submenu index="5">
-      <template v-slot:title>我的工作台</template>
-      <el-menu-item index="5-1">选项1</el-menu-item>
-    </el-submenu>
-  </el-menu>
+  <div></div>
 </template>
 
-<script>
-import { ref } from 'vue'
+<script lang="ts">
+/**
+ * Used to test individual components
+ */
 export default {
   setup() {
-    let activeIndex = ref('1')
-    let activeIndex2 = ref('1')
-
-    function handleSelect(key, keyPath) {
-      console.log(key, keyPath)
-    }
-
-    return { activeIndex, activeIndex2, handleSelect }
+    return {}
   }
 }
 </script>
