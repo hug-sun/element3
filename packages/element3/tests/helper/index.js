552b810e5b63b431bb916c28ff3643f1a83beca9
cuixiaorui <cui_xiaorui@126.com>
2021-03-06 15:44:34 +0800

    chore: remove jest-vtu

--- packages/element3/tests/helper/index.js
+++ packages/element3/tests/helper/index.js
@@ -3,42 +3,43 @@
 // 后面需要用 tlv 的写法把这些方法都替换掉
 
 export function expectHaveClass(wrapper, className) {
-  expect(wrapper).toHaveClass(className)
+  expect(wrapper.classes()).toContain(className)
 }
 
 export function expectNotHaveClass(wrapper, className) {
-  expect(wrapper).not.toHaveClass(className)
+  expect(wrapper.classes()).not.toContain(className)
 }
 
 export function expectHaveAttribute(wrapper, key, value) {
   if (arguments.length === 2) {
-    expect(wrapper).toHaveAttribute(key)
+    expect(wrapper.attributes()).toHaveProperty(key)
     return
   }
 
-  expect(wrapper).toHaveAttribute(key, value)
+  expect(wrapper.attributes(key)).toBe(value)
 }
 
 export function expectNotHaveAttribute(wrapper, key, value) {
   if (arguments.length === 2) {
-    expect(wrapper).not.toHaveAttribute(key)
+    expect(wrapper.attributes()).not.toHaveProperty(key)
     return
   }
-  expect(wrapper).not.toHaveAttribute(key, value)
+
+  expect(wrapper.attributes(key)).toBe(value)
 }
 
 export function expectHaveStyle(wrapper, styleInfo) {
-  expect(wrapper).toHaveStyle(styleInfo)
+  expect(wrapper.element.style).toMatchObject(styleInfo)
 }
 
 export function expectToBeExist(wrapper) {
-  expect(wrapper).toBeExist()
+  expect(wrapper.exists()).toBe(true)
 }
 
 export function expectNotToBeExist(wrapper) {
-  expect(wrapper).not.toBeExist()
+  expect(wrapper.exists()).toBe(false)
 }
 
 export function expectHaveTextContent(wrapper, content) {
-  expect(wrapper).toHaveTextContent(content)
+  expect(wrapper.text()).toContain(content)
 }
