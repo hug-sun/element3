c1b5b9ed83c3891ef7a8734de1145822d654865c
花果山大圣 <316783812@qq.com>
2022-06-19 00:59:24 +0800

    chore: update snapshot and eslint fix

--- src/components/divider/Divider.vue
+++ src/components/divider/Divider.vue
@@ -1,29 +1,27 @@
 <script setup lang="ts">
-
 import { withDefaults } from 'vue'
 interface DividerType {
-  direction?:'horizontal'|'vertical'
-  contentPosition?:'left'|'center'|'right'
-  dividerStyle?:'dotted'|'dashed'|'double'|'solid'
-  dark?:boolean
-  color?:string
+  direction?: 'horizontal' | 'vertical'
+  contentPosition?: 'left' | 'center' | 'right'
+  dividerStyle?: 'dotted' | 'dashed' | 'double' | 'solid'
+  dark?: boolean
+  color?: string
 }
 
-withDefaults(defineProps<DividerType>(),{
-  direction:'horizontal',
-  contentPosition:'center',
-  dividerStyle:'solid',
-  dark:false,
-  color:''
+withDefaults(defineProps<DividerType>(), {
+  direction: 'horizontal',
+  contentPosition: 'center',
+  dividerStyle: 'solid',
+  dark: false,
+  color: '',
 })
-
 </script>
 
 <template>
-  <div v-bind="$attrs" :class="['el-divider', `el-divider--${direction}`, `is-${dividerStyle}`]" :style="{color}">
+  <div v-bind="$attrs" class="el-divider" :class="[`el-divider--${direction}`, `is-${dividerStyle}`]" :style="{ color }">
     <div
       v-if="$slots.default && direction !== 'vertical'"
-      :class="['el-divider__text', `is-${contentPosition}`, dark?'is-dark':'']"
+      class="el-divider__text" :class="[`is-${contentPosition}`, dark ? 'is-dark' : '']"
     >
       <slot />
     </div>
