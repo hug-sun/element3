296f6f3c92b015b079882c601e1d6f6704057776
zer0fire <1076671121@qq.com>
2022-06-19 16:40:54 +0800

    fix: 修改 lint 问题

--- src/components/avatar/Avatar.vue
+++ src/components/avatar/Avatar.vue
@@ -1,58 +1,40 @@
-<template>
-  <span v-if="!icon" class="el-avatar" :style="avatarStyle" :class="classes">
-    <img
-      v-if="(src || srcset) && !errorFlag" 
-      @error="errorHandler($event)" 
-      :src="src"
-      :srcset="srcset"
-      :alt="alt"
-      :style="{ objectFit: fit }"
-    />
-    <slot v-else></slot>
-  </span>
-  <span v-else-if="typeof icon === 'string'" class="el-avatar" :style="avatarStyle" :class="classes">
-    <el-icon :name="icon" />
-  </span>
-  <span  v-else class="el-avatar" :style="avatarStyle" :class="classes">
-    <slot></slot>
-  </span>
-</template>
 <script lang="ts">
-import { defineComponent } from 'vue';
+import { defineComponent } from 'vue'
+import { computed, ref, watch, withDefaults } from 'vue'
+import type { Component } from 'vue'
+import { ElIcon } from '../icon/index'
 export default defineComponent({
-  name: 'ElAvatar'
+  name: 'ElAvatar',
 })
 </script>
-<script setup lang="ts">
-import { Component, withDefaults, computed, ref, watch } from 'vue';
-import { ElIcon } from '../icon/index';
 
+<script setup lang="ts">
 type AvatarSize = 'large' | 'medium' | 'small'
 type AvatarFitType = 'fill' | 'contain' | 'cover' | 'none' | 'scale-down'
 type AvatarShapeType = 'circle' | 'square'
 
 interface AvatarProps {
-  icon: string | Component,
-  size?: number | AvatarSize,
-  shape?: AvatarShapeType,
-  src?: string,
-  srcset?: string,
-  alt?: string,
-  fit?: AvatarFitType,
+  icon?: string | Component
+  size?: number | AvatarSize
+  shape?: AvatarShapeType
+  src?: string
+  srcset?: string
+  alt?: string
+  fit?: AvatarFitType
 }
 
 interface AvatarEmits {
-  error: (e: Event) => Event;
+  error: (e: Event) => Event
 }
 
-const emit = defineEmits([ 'error' ])
-const props = withDefaults(defineProps<AvatarProps>(),{
+const props = withDefaults(defineProps<AvatarProps>(), {
   size: 'medium',
   shape: 'circle',
   fit: 'contain',
 })
+const emit = defineEmits(['error'])
 
-const useClasses = function(props: AvatarProps){
+const useClasses = function (props: AvatarProps) {
   const shape = props.shape
   const size = props.size
   const icon = props.icon
@@ -63,15 +45,15 @@ const useClasses = function(props: AvatarProps){
         ? typeof size === 'string'
           ? `el-avatar--${size}`
           : ''
-        : `el-avatar--medium`,
-      icon ? `el-avatar--icon` : ''
+        : 'el-avatar--medium',
+      icon ? 'el-avatar--icon' : '',
     ]
   })
 }
 const useAvatarStyle = function (props: AvatarProps) {
   const size = props.size
   return computed(() => {
-    return Number.isNaN(Number(size)) ? '' : { height: size + 'px', width: size + 'px' }
+    return Number.isNaN(Number(size)) ? '' : { height: `${size}px`, width: `${size}px` }
   })
 }
 const useFitStyle = function (props: AvatarProps) {
@@ -92,8 +74,27 @@ const errorHandler = (e: Event) => {
   errorFlag.value = true
   emit('error', e)
 }
-
 </script>
+
+<template>
+  <span v-if="!icon" class="el-avatar" :style="avatarStyle" :class="classes">
+    <img
+      v-if="(src || srcset) && !errorFlag"
+      :src="src"
+      :srcset="srcset"
+      :alt="alt"
+      :style="{ objectFit: fit }"
+      @error="errorHandler($event)"
+    />
+    <slot v-else />
+  </span>
+  <span v-else-if="typeof icon === 'string'" class="el-avatar" :style="avatarStyle" :class="classes">
+    <el-icon :name="icon" />
+  </span>
+  <span v-else class="el-avatar" :style="avatarStyle" :class="classes">
+    <slot />
+  </span>
+</template>
 
 <style lang="scss">
 @import '../../theme/src/avatar.scss';
--- src/components/avatar/__test__/avatar.spec.ts
+++ src/components/avatar/__test__/avatar.spec.ts
@@ -1,45 +1,44 @@
-import Avatar from '../Avatar.vue'
 import { mount } from '@vue/test-utils'
-import { render, getByRole } from '@testing-library/vue'
+import Avatar from '../Avatar.vue'
 import { ElIcon } from '../../icon'
 
 describe('Avatar.vue', () => {
   it('snapshot', () => {
     const wrapper = mount(Avatar, {
-      props: { icon: 'user-solid' }
+      props: { icon: 'user-solid' },
     })
     expect(wrapper.find('.el-avatar').exists()).toBe(true)
   })
   it('content', () => {
     const vm = mount(Avatar, {
       slots: {
-        default: () => 'Im perfect avatar!'
-      }
+        default: () => 'Im perfect avatar!',
+      },
     })
     expect(vm.text()).toBe('Im perfect avatar!')
   })
   it('size', () => {
     const vm = mount(Avatar, {
-      props: { icon: 'user-solid', size: 'large' }
+      props: { icon: 'user-solid', size: 'large' },
     })
     expect(vm.classes()).toContain('el-avatar--large')
   })
   it('shape', () => {
     const vm = mount(Avatar, {
-      props: { icon: 'user-solid', shape: 'square' }
+      props: { icon: 'user-solid', shape: 'square' },
     })
     expect(vm.classes()).toContain('el-avatar--square')
   })
   it('fit', () => {
     const url = 'https://cdn.jsdelivr.net/gh/shengxinjing/static/element3.png'
     const vm = mount(Avatar, {
-      props: { src: url, fit: 'none' }
+      props: { src: url, fit: 'none' },
     })
-    expect(vm.find('img').attributes('style')).toContain(`object-fit: none;`)
+    expect(vm.find('img').attributes('style')).toContain('object-fit: none;')
   })
   it('icon', () => {
     const vm = mount(Avatar, {
-      props: { icon: 'user-solid' }
+      props: { icon: 'user-solid' },
     })
     expect(vm.classes()).toContain('el-avatar--icon')
     expect(vm.findComponent(ElIcon).exists()).toBe(true)
